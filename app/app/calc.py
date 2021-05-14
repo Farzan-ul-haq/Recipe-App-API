016af45e3f7fb66f74a4838561e15ef745a4843f
farzan-ul-haq <farzanulhaq123@gmail.com>
2021-05-14 17:21:21 +0500

    unit test & core app

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+
+
+def add(x, y):
+    '''Add Two numbers together'''
+    return x + y
+
+def sub(x, y):
+    '''Subract Two numbers together'''
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from app.calc import add, sub
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3, 8), 11)
+        
+    def test_subract_numbers(self):
+        """Test that two numbers are subracted together"""
+        self.assertEqual(sub(10, 5), -5)
