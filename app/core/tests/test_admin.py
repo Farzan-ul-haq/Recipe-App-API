60521c2eac028cf59a35efabac6b0d16932a4467
farzan-ul-haq <farzanulhaq123@gmail.com>
2021-05-18 10:22:12 +0500

    feat: create user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -38,3 +38,10 @@ class AdminSiteTests(TestCase):
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test taht the create user page works"""
+        url = reverse('admin:core_user_add')
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
