5b36b50ada8cfbe1a8602a51f6d7e84010bac71b
farzan-ul-haq <farzanulhaq123@gmail.com>
2021-05-31 12:01:52 +0500

    feat: Recipe Model

--- app/core/models.py
+++ app/core/models.py
@@ -46,3 +46,20 @@ class Ingredient(models.Model):
 
     class Meta:
         ordering = ['-name']
+
+
+class Recipe(models.Model):
+    """Recipe Object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
